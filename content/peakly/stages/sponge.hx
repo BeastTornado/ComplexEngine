5da2e62a114825dafff1c8757fe63a17aa44569a
BeastTornado <littleman06.qy@gmail.com>
2025-04-24 20:07:26 -0400

    PENDED

--- content/peakly/stages/sponge.hx
+++ content/peakly/stages/sponge.hx
@@ -3,6 +3,8 @@ var mainBG2:FlxSprite;
 var mainBG1:FlxSprite;
 
 var sponge:BGSprite;
+var flashlight:BGSprite;
+var black:BGSprite;
 
 var float:Int = 1;
 
@@ -22,6 +24,16 @@ function onLoad(){
     sponge = new BGSprite('stages/sponge/spongebob1', 150, 100, 0.8, 0.8);
     sponge.scale.set(0.3, 0.3);
     add(sponge);
+
+    flashlight = new BGSprite('stages/sponge/flashlight', -370, -200, 0.25, 0.25);
+    flashlight.scale.set(0.8, 0.8);
+    add(flashlight);
+
+    black = new BGSprite('stages/sponge/black', -370, -200, 0.25, 0.25);
+    black.scale.set(0.8, 0.8);
+    add(black);
+
+    //i poop
 }
 
 function onCreatePost(){
@@ -29,6 +41,8 @@ function onCreatePost(){
     game.addBehindBF(mainBG2);
     game.addBehindBF(mainBG1);
     game.addBehindBF(sponge);
+    game.addBehindBF(flashlight);
+    game.addBehindBF(black);
 
     game.playHUD.healthBar.visible = false;
     game.playHUD.iconP1.visible = false;
@@ -40,18 +54,18 @@ function onCreatePost(){
 }
 
 function onUpdate(elapsed){
-
+    black.alpha = 1-(game.health/0.5);
 }
 
 function onBeatHit(){
-    if ((curBeat % 8) != 0) return;
+    if ((curBeat % 4) != 0) return;
     if(float == 1){
-        FlxTween.tween(sponge, {y: 115}, 2.6, {
+        FlxTween.tween(sponge, {y: 115}, 2.4, {
             ease: FlxEase.quadInOut
         });
         float = -1;
     } else {
-        FlxTween.tween(sponge, {y: 85}, 2.6, {
+        FlxTween.tween(sponge, {y: 85}, 2.4, {
             ease: FlxEase.quadInOut
         });
         float = 1;
